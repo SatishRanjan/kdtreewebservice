839067ad9a621b3f65887d2d4ef2bcd22f9970d9
Satish Ranjan <satishra@microsoft.com>
2021-04-18 22:24:12 -0700

    adding more tests

--- src/test/java/finalproject5278/KDStoreTest.java
+++ src/test/java/finalproject5278/KDStoreTest.java
@@ -14,7 +14,7 @@ public class KDStoreTest {
 		boolean result = kdStore.search(coordinate1);
 		assertEquals(true, result);
 	}
-	
+
 	@Test
 	public void testThreeDimenasionalInsert() {
 		KdTreeStore kdStore = new KdTreeStore(3);
@@ -24,5 +24,16 @@ public class KDStoreTest {
 		kdStore.insert(coordinate2);
 		boolean result = kdStore.search(coordinate1);
 		assertEquals(true, result);
+	}
+
+	@Test
+	public void testCoordinateSearch() {
+		KdTreeStore kdStore = new KdTreeStore(3);
+		int[] coordinate1 = { 2, 10, 5 };
+		kdStore.insert(coordinate1);
+		boolean result = kdStore.search(coordinate1);
+		assertEquals(true, result);
+		boolean result1 = kdStore.search(new int[] { 20, 10, 25 });
+		assertEquals(false, result1);
 	}
 }
