1b190ebcaf8ba695a53a4b2686e354ca4532fd28
Satish Ranjan <satishra@microsoft.com>
2021-04-19 09:56:53 -0700

    Adding command line arguments for the k-d tree dimension

--- src/main/java/finalproject5278/HttpServer.java
+++ src/main/java/finalproject5278/HttpServer.java
@@ -8,10 +8,21 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class HttpServer {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
+		int dimensionCount = 3;
+		if (args != null && args.length > 0) {
+			for (String arg : args) {
+				System.out.println("Args:" + arg);
+			}
+
+			if (Utils.isInt(args[0])) {
+				dimensionCount = Integer.parseInt(args[0]);
+			}
+		}
+
 		int port = 8080;
 		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
 		ServerSocket serverSocket = new ServerSocket(port);
-		KdStoreConfigManager.Configure(3);
+		KdStoreConfigManager.Configure(dimensionCount);
 		System.out.println("K-d web sever started on port:" + port);
 		while (true) {
 			Socket clientSocket = serverSocket.accept();
