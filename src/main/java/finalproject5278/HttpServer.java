fac51373bb4538cf7727b709379666ee773abd30
Satish Ranjan <satishra@microsoft.com>
2021-04-19 10:10:42 -0700

    making k-d webserver port as command line arg

--- src/main/java/finalproject5278/HttpServer.java
+++ src/main/java/finalproject5278/HttpServer.java
@@ -9,21 +9,26 @@ public class HttpServer {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 		int dimensionCount = 3;
+		int webServerPort = 8080;
 		if (args != null && args.length > 0) {
 			for (String arg : args) {
 				System.out.println("Args:" + arg);
 			}
 
-			if (Utils.isInt(args[0])) {
-				dimensionCount = Integer.parseInt(args[0]);
+			String[] argsSplit = args[0].split(",");
+			if (Utils.isInt(argsSplit[0])) {
+				dimensionCount = Integer.parseInt(argsSplit[0]);
+			}
+
+			if (argsSplit.length > 1 && Utils.isInt(argsSplit[1])) {
+				webServerPort = Integer.parseInt(argsSplit[1]);
 			}
 		}
 
-		int port = 8080;
 		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-		ServerSocket serverSocket = new ServerSocket(port);
+		ServerSocket serverSocket = new ServerSocket(webServerPort);
 		KdStoreConfigManager.Configure(dimensionCount);
-		System.out.println("K-d web sever started on port:" + port);
+		System.out.println("K-d web sever started on port:" + webServerPort);
 		while (true) {
 			Socket clientSocket = serverSocket.accept();
 			System.out.println("Client request received: " + clientSocket.toString());
